412b505b297cf2ad4cb3811c3f3d5fb7929554b9
wabriones <warrenkarl.briones@gmail.com>
2016-10-05 22:40:46 +0800

    He

---
+++ AndroidTest/Resources/Objects/Questions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Questions
+    {
+        public int QuestionID { get; set; }
+
+        public string Question { get; set; }
+
+        public List<Choices> Choices { get; set; }
+    }
+
+    
+}
